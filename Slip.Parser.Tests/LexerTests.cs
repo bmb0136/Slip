7a18ef12b9e44aa746aae1a985d5ee2c97b37be2
Brandon Buckley <brandonmb0136@gmail.com>
2024-09-29 12:39:55 -0500

    Lex string tests

--- Slip.Parser.Tests/LexerTests.cs
+++ Slip.Parser.Tests/LexerTests.cs
@@ -16,4 +16,17 @@ public class LexerTests
     Assert.Equal(1, tokens.Count);
     Assert.Equal(type, tokens[0].Type);
   }
+
+  [Theory]
+  [InlineData("\"hello\"", "hello")]
+  [InlineData("\"test\\n123\"", "test\n123")]
+  public void Lex_String_ReturnsCorrectValue(string code, string value)
+  {
+    var (tokens, error) = Lexer.Lex(code);
+
+    Assert.Null(error);
+    Assert.Equal(1, tokens.Count);
+    Assert.Equal(value, tokens[0].Value);
+  }
+
 }
